e59832cd92d019b0d8f7141ddb86c177c29304df
Albert De La Cruz <adelac10@asu.edu>
2019-10-08 19:44:51 -0700

    Initial commit of AddingMachine

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,31 @@
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString () {
+		return "";
+	}
+	
+	public void clear() {
+		
+	}
+}
+
