558c823e78c43350a09719b4d0c89dd9dc2d8a85
Albert De La Cruz <adelac10@asu.edu>
2019-10-08 21:23:36 -0700

    Creating the initial version

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -2,6 +2,10 @@ package cse360assign2;
 
 /**
  * 
+ * 
+ * 
+ * ID: 1213265207
+ * Assignment 2 
  * @author Albert De La Cruz
  * 
  */
@@ -9,26 +13,49 @@ public class AddingMachine {
 	
 	private int total;
 	
+	/**
+	 * Default constructor, sets total 0
+	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Getter for variable total
+	 * @return total Returns the variable total
+	 */
 	public int getTotal () {
 		return 0; 
 	}
 	
+	/**
+	 * Adds the parameter value to the total internal variable
+	 * @param value
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/**
+	 * Subtracts the parameter value from the total variable
+	 * @param value User inputed value to subtract from
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	/**
+	 * outputs all of the transactions and operations done to the total variable
+	 * as a string with + for add and - for subtract
+	 * @returns outputString returns the total transactions and operations as a string
+	 */
 	public String toString () {
 		return "";
 	}
 	
+	/**
+	 * 
+	 */
 	public void clear() {
 		
 	}
